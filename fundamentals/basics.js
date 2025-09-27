44883e0f431e47c66dac7958d58bacdae46acb79
root <root@TheDesktop.localdomain>
2025-09-27 12:49:52 -0700

    new code

--- fundamentals/basics.js
+++ fundamentals/basics.js
@@ -2,3 +2,23 @@
 // Const you cannot change the value 
 // var no real situation to use it these days there are differences in scoping
 
+//slice will slice starting at the first value and then will stop the slice. The stop is before.
+
+console.log("Hello World");
+console.log("Hello Again");
+
+// if (1 !== 1) {
+//     console.log("ITS TRUE");
+// }
+
+let rating = 3
+
+if (rating === 3) {
+    console.log("You are a superstar");
+}
+
+let num = 37
+
+if (num % 2 !== 0){
+    console.log("Odd Number");
+}
