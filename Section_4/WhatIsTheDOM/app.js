2ed4c7ae55e71a00a33e79943b830ed2a82e5fec
root <root@TheDesktop.localdomain>
2025-10-02 21:24:27 -0700

    more js code

--- Section_4/WhatIsTheDOM/app.js
+++ Section_4/WhatIsTheDOM/app.js
@@ -1,1 +1,13 @@
-document.getElementById('gallery');
+//GET ELEMENT BY ID
+//ALL START WITH document.getSomething()
+
+//<form id="muffin-form"> ID = MUFFIN-FORM
+
+//FINDS FIRST ELEMENT
+//YOU GET AN OBJECT BACK WITH PROPERTIES AND METHODS
+document.getElementById('content');
+
+
+//GET ELEMENTS BY TAG NAME
+
+document.getElementsByTagName('h2')
