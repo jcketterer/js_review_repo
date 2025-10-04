8887ec3da73020a7d83ed58d54bcde133aafdaa7
root <root@TheDesktop.localdomain>
2025-10-03 19:18:05 -0700

    adding 4.2 section files

--- Section_4/WhatIsTheDOM/app.js
+++ Section_4/WhatIsTheDOM/app.js
@@ -10,4 +10,40 @@ document.getElementById('content');
 
 //GET ELEMENTS BY TAG NAME
 
-document.getElementsByTagName('h2')
+document.getElementsByTagName('img');
+
+//GET ELEMENT BY CLASS NAME
+
+document.getElementsByClassName('latin');
+
+//QUERY SELECTOR
+//USING A # WILL SELECT ANY ID WITH THE NAME FOLLOWING IT 
+
+document.querySelector('#main');
+
+//USING multiple selectors with element.class to be more specific
+
+document.querySelector('h2.section-heading');
+
+document.querySelector('#submit')
+document.querySelector('h2')
+document.querySelector('.section-title')
+document.querySelector('h3.section-title')
+document.querySelector('input')
+document.querySelector('input[type="text"]')
+document.querySelector('input[type="range"]')
+document.querySelectorAll('input')
+
+document.querySelectorAll('h2')
+
+document.querySelectorAll('h3')
+
+document.querySelectorAll('h3.section-title.country')
+
+document.querySelectorAll('hr')
+
+document.querySelectorAll('body > hr')
+
+document.querySelectorAll(':not(p)')
+
+document.querySelectorAll('h2:nth-of-type(3)')
